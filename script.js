38f4c3a2ca3a24b195839783949014fc1e82639f
Yash Bhawsar <104437573+YashBhawsar23@users.noreply.github.com>
2024-05-12 00:56:44 +0530

    Added a new feature
    Added a new function, which will change the order of Questions everytime page reloadd

--- script.js
+++ script.js
@@ -186,4 +186,17 @@ nextButton.addEventListener("click", () => {
   }
 });
 
+
+
+function shuffleQuestions() {
+    for (let i = questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+  }
+
+
+
+  shuffleQuestions();  
+
 startQuiz();
